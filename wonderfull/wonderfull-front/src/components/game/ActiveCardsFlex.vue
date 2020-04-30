670ef21161ad5b182512da797047f019357eec58
Pierre-Alain Keyser <pakeyser@groupeastek.fr>
2020-04-30 16:24:18 +0200

    Suppression de l'effet de clignotement quand on change d'empire

--- wonderfull/wonderfull-front/src/components/game/ActiveCardsFlex.vue
+++ wonderfull/wonderfull-front/src/components/game/ActiveCardsFlex.vue
@@ -2,7 +2,7 @@
 <div>
   <span class="font-weight-light caption">{{title}}</span>
   <div class="d-flex flex-wrap align-start">
-    <CardView v-for="(c,i) in cards" :key="i+'card'" :card="c" @action="action" />
+    <CardView v-for="c in cards" :key="c.id" :card="c" @action="action" />
   </div>
 </div>
 </template>
